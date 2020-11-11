3a88c444db3d984772e85c0057270fdf12303c4e
HZQ <15022673515@163.com>
2020-11-11 22:48:09 +0800

    master

--- 数据结构与算法/线性表/动态分配.cpp
+++ 数据结构与算法/线性表/动态分配.cpp
@@ -1,6 +1,40 @@
-#define InitSize 10 // 表长度的初始定义
+#include <stdio.h>
+#include <stdlib.h>
+#define InitSize 10 /* 表顺序表的初始长度 */
+
 typedef struct
 {
-    int *data;           // 指示动态分配数组的指针,ElemType是数据类型，如int，double等
-    int MaxSize, length; // 顺序表的最大容量和当前长度
-} SeqList;
+    int *data;           /* 指示动态分配数组的指针，指向数据表第一个元素；ElemType是数据类型，如int，double等 */
+    int MaxSize, length; /* 顺序表的最大容量和当前长度 */
+} SeqList;               /* 顺序表的类型定义(动态分配方式) */
+
+void InitList(SeqList &L)
+{
+    /* 用malloc函数申请一片连续的存储空间 */
+    L.data = (int *)malloc(InitSize * sizeof(int));
+    L.length = 0;
+    L.MaxSize = InitSize;
+}
+
+/* 增加动态数组的长度 */
+void IncreaseSize(SeqList &L, int len)
+{
+    int *p = L.data;
+    L.data = (int *)malloc((L.MaxSize + len) * sizeof(int));
+    for (int i = 0; i < L.length; i++)
+    {
+        L.data[i] = p[i]; /* 将数据复制到新区域 */
+    }
+    L.MaxSize = L.MaxSize + len;
+    free(p);
+}
+
+int main()
+{
+    SeqList L;   /* 声明 */
+    InitList(L); /* 初始化 */
+    /* 之后随意插入几个元素 */
+
+    IncreaseSize(L, 5);
+    return 0;
+}
--- 数据结构与算法/线性表/静态分配.cpp
+++ 数据结构与算法/线性表/静态分配.cpp
@@ -1,7 +1,24 @@
 #include <stdio.h>
-#define MaxSize 10 // 定义线性表的最大长度
+#define MaxSize 10 /* 定义线性表的最大长度 */
 typedef struct
 {
-    int data[MaxSize]; // 顺序表的元素,ElemType
-    int length;        // 顺序表的当前长度
+    int data[MaxSize]; /* 顺序表的元素,ElemType */
+    int length;        /* 顺序表的当前长度 */
 } SqList;
+
+/* 基本操作--初始化一个顺序表 */
+void InitList(SqList &L)
+{
+    for (int i = 0; i < MaxSize; i++)
+    {
+        L.data[i] = 0; /* 将所有数据元素设置为默认初始值(可省略) */
+    }
+    L.length = 0; /* 顺序表初始长度为0(不能忽略) */
+}
+
+int main(void)
+{
+    SqList L;    /* 声明一个顺序表 */
+    InitList(L); /* 调用函数，初始化顺序表 */
+    return 0;
+}
